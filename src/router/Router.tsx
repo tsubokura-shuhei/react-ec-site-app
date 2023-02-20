6a2f4396dcfee7a4ec24f417a693fff50331cee0
tsubokura-shuhei <repeat2639@gmail.com>
2023-02-20 23:14:41 +0900

    first comit

---
+++ src/router/Router.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route></Route>
+    </Switch>
+  );
+};
